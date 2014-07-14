c8cb12e44fd895121292c7e605a5dbd969a69850
Hyungki <Hyungki@users.noreply.github.com>
2014-07-14 08:46:43 -0400

    Update Clase1.java

--- Clase1/src/clase1/Clase1.java
+++ Clase1/src/clase1/Clase1.java
@@ -16,7 +16,7 @@ public class Clase1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("Hola Mundo");
     }
     
 }
